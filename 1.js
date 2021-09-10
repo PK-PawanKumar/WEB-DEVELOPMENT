ef7587ecf9ac24e93b651af781338d777f4238bf
Pawan Kumar <pawan.kumar.ece20@itbhu.ac.in>
2021-09-10 23:45:58 +0530

    output methods

---
+++ 1.js
@@ -0,0 +1,16 @@
+function myFunction1()
+{
+    document.getElementById("a").innerHTML = "innerHTML output code";
+}
+function myFunction2()
+{
+    document.write(3+4);
+}
+function myFunction3()
+{
+    window.alert("alert ! , u clicked on tab");
+}
+function myFunction4()
+{
+    console.log(2+4);
+}
