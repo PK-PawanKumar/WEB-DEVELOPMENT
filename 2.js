7e89e664d83608fb997b15876412ab25796dd503
Pawan Kumar <pawan.kumar.ece20@itbhu.ac.in>
2021-09-18 14:46:32 +0530

    axios using .json file

---
+++ 2.js
@@ -0,0 +1,60 @@
+// const { default: axios } = require("axios");
+
+document.getElementById("btn").addEventListener("click", make_req);
+
+function make_req() {
+    console.log("button clicked!")
+    // config = {
+    //     method: 'get',
+    //     url: 'text.txt'
+    // }
+    // const promeiseobj = axios(config)
+    // promeiseobj.then((res) => {
+    //     console.log(res)
+    //     console.log(res.data)
+    // })
+
+    //// getting error
+    // config = {
+    //     method:'get',
+    //     url:'temxt.txt'
+    // }
+    // const promeiseobj = axios(config)
+    // promeiseobj.then((res)=>{
+    //     console.log(res)
+    //     console.log(res.data)
+    //     document.getElementById("txt_file_data").innerText=res.data
+    // }).catch((error)=>{
+    //     console.log(error)
+    // })
+
+
+    // showing data in browser
+
+    // config = {
+    //     method: 'get',
+    //     url: 'text.txt'
+    // }
+    // const promeiseobj = axios(config)
+    // promeiseobj.then((res) => {
+    //     console.log(res)
+    //     console.log(res.data)
+    //     document.getElementById("txt_file_data").innerText = res.data
+    // })
+
+
+    // using json file
+    config = {
+        method: 'get',
+        url: 'text.json'
+    }
+    const promeiseobj = axios(config)
+    promeiseobj.then((res) => {
+        console.log(res)
+        console.log("id = " + res.data.id)
+        console.log("name = " + res.data.name)
+        document.getElementById("txt_file_data").innerText = res.data
+    })
+
+
+}
